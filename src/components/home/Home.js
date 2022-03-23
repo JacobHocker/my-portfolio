17c9ffe2953dd738115580d84568196266177253
JacobHocker <jacobdouglas06@gmail.com>
2022-03-23 16:22:55 -0500

    removed cards

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import './Home.scss';
 
 function Home({ isDark }) {
@@ -28,34 +28,6 @@ function Home({ isDark }) {
                         </div>
                     </Col>
                 </Row> 
-                {/* <Row className="home-row-three">
-                    <Col className={isDark === true ? "dk-row-three" : "lt-row-three"}>
-                        <div className={isDark === true ? "dk-card-container" : "lt-card-container"}>
-                            <Card className={isDark === true ? "dk-card" : "lt-card"} >
-                                <Card.Img className='card-image' variant="top" src="https://spunout.ie/wp-content/uploads/elementor/thumbs/ThinkstockPhotos-625736338-2-p8t2sjb2vwkob00ipqxwab2ww22gm1kz3er3vqrovs.jpg" />
-                                <Card.Body>
-                                    <Card.Title><h1>Team Player</h1></Card.Title>
-                                    <Card.Text>
-                                       <p>All about enabling a team or group to excel and deliver!</p> 
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    </Col>
-                    <Col className={isDark === true ? "dk-row-three" : "lt-row-three"}>
-                        <div className={isDark === true ? "dk-card-container" : "lt-card-container"}>
-                            <Card className={isDark === true ? "dk-card" : "lt-card"} >
-                                <Card.Img className='card-image' variant="top" src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_504013680_135336.jpg" />
-                                <Card.Body>
-                                    <Card.Title><h1>Detail Oriented</h1></Card.Title>
-                                    <Card.Text>
-                                       <p>Always better to do things right the first time!</p> 
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    </Col>
-                </Row> */}
                 <Row className="home-row-four">
                     <Col className={isDark === true ? "dk-row-four-col-one" : "lt-row-four-col-one"}>
                         <div className={isDark === true ? "dk-intro-container" : "lt-intro-container"}>
